aadcd97d4073a82ada14ec1b078e9841a6c0512a
ayune-6 <ayune19990823@gmail.com>
2020-06-18 07:12:55 +0000

    commit

--- resources/views/layouts/basic.blade.php
+++ resources/views/layouts/basic.blade.php
@@ -10,22 +10,21 @@
 
         	<title>@yield('title')</title>
         	{{-- Javascript読み込み --}}
-        	<script src="{{ secure_asset('js/app.js') }}" defer></script>
+        	<script src="{{ asset('js/app.js') }}" defer></script>
 
         	{{-- Fonts --}}
         	<link rel="dns-prefetch" href="https://fonts.gstatic.com">
-        	<link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
-
+		<link href="https://fonts.googleapis.com/css2?family=Raleway&display=swap" rel="stylesheet">
         	{{-- Laravelで用意されているstylesheetの読み込み --}}
-        	<link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
+        	<link href="{{ asset('css/app.css') }}" rel="stylesheet">
         	{{-- このあと作成するbasic CSSの読み込み --}}
-        	<link href="{{ secure_asset('css/basic.css') }}" rel="stylesheet">
+        	<link href="{{ asset('css/basic.css') }}" rel="stylesheet">
 	</head>
 	<body>
         	<div id="app">
             		{{-- ナビゲーションバー --}}
-            		<nav class="navbar navbar-expand-lg navbar-dark">
-				<div class="container">
+			<div class="container">
+            			<nav class="navbar navbar-light navbar-expand-lg" style="background-color: #ffffff">
            	 			<a class="navbar-brand" href="{{ url('/') }}">mupo</a>
                  			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                  				<span class="navbar-toggler-icon"></span>
@@ -33,17 +32,17 @@
 
                  			<div class="collapse navbar-collapse" id="navbarSupportedContent">
                  				<ul class="navbar-nav mr-auto">
-		 					<li class="nav-item"><a class="nav-link" href="#">Home</a></li>
-							<li class="nav-item"><a class="nav-link" href="#">Notification</a></li>
+		 					<li class="nav-item"><a class="nav-link" href="{{ url('/home') }}">Home</a></li>
+							<li class="nav-item"><a class="nav-link" href="{{ url('/notifications') }}">Notification</a></li>
 							<li class="nav-item"><a class="nav-link" href="#">Profile</a></li>
 		 				</ul>
 		 				<form class="form-inline my-2 my-lg-0">
      							<input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-     							<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+     							<button class="btn btn-dark" type="submit">Search</button>
    		 				</form>
                  			</div>
-				</div>
-            		</nav>
+				</nav>
+            		</div>
             		{{-- ナビゲーションバー終わり --}} 
             		<main class="py-4">
                 		@yield('content')
