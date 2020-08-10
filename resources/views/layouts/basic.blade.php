43248ddcfb89f3257f9664bca576701af392fd6f
ayune-6 <ayune19990823@gmail.com>
2020-08-10 06:08:05 +0000

    secure

--- resources/views/layouts/basic.blade.php
+++ resources/views/layouts/basic.blade.php
@@ -22,9 +22,9 @@
         	<link rel="dns-prefetch" href="https://fonts.gstatic.com">
 			<link href="https://fonts.googleapis.com/css2?family=Raleway&display=swap" rel="stylesheet">
         	{{-- Laravelで用意されているstylesheetの読み込み --}}
-        	<link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        	<link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
         	{{-- このあと作成するbasic CSSの読み込み --}}
-			<link href="{{ asset('css/basic.css') }}" rel="stylesheet">
+			<link href="{{ secure_asset('css/basic.css') }}" rel="stylesheet">
 			
 			<link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
 			<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
