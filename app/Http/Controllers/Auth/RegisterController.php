f98cff5c8ca0ed2915c86da7d9c7bc8a60ab931b
ayune-6 <ayune19990823@gmail.com>
2020-08-11 06:32:20 +0000

    verify2

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -105,7 +105,11 @@ class RegisterController extends Controller
         
     }
 
-    
+    protected function registered(Request $request, $user)
+    {
+        $this->guard()->logout();
+        return redirect('/login')->with('status', 'We sent you an activation code. Check your email and click on the link to verify.');
+    }
 
     
 }
